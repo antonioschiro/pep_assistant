2dd6fe73eb6664afed6aea6e45b2230c043e57ab
antonioschiro <antonioschiro21@gmail.com>
2025-05-23 12:08:03 +0200

    Correctness evaluator updated

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -48,37 +48,85 @@ def predict_rag_context_response(example:dict) -> dict:
     except Exception as e:
         print(f"An unknown exception occurred:\n{e}")
 
-# Using two evaluators to estimate the response correctness 
-correctness_evaluator = LangChainStringEvaluator(
-        "labeled_criteria",
-        prepare_data= lambda run, example: {
-            "prediction": run.outputs["answer"],
-            "reference": example.outputs["answer"],
-            "input": example.inputs["question"],
-        },
-        config={
-        "llm": llm,
-        "criteria": {
-            "correctness": '''Is the predicted code equivalent to the reference provided? 
-            Definition of equivalence:
-            - the predicted code has the same structure of the reference code;
-            - the predicted code implements the same logical steps or algorythm of the reference code;
-            - the predicted code produces the same output of the reference code given the same input.
 
-            What must *not* influence the correctness:
-            - variable names, even if they affect the code output;
-            - minor differences in the structure that doesn't change the meaning (e.g. using a loop instead of a list comprehension)
-            
-            Rate correctness on a scale from 0 to 10:
-             0 = incorrect logic or failure to implement the intended functionality
-             6 = Mostly equivalent but with minor logical inconsistencies
-             10 = Fully equivalent in logic and behaviour
-            Return the score calculated (0-10).
-            ''',
-        },
-        }
-    )
+# Evaluators
+from langsmith.schemas import Run, Example
+from langchain.prompts import PromptTemplate
+from pydantic import BaseModel
+from typing import Annotated
+class GroundnessScore(BaseModel):
+    score: Annotated[int, "Groundness score from 0 to 10"]
+    reasoning: Annotated[str, "Reasoning behind the score."]
 
+class AdherenceScore(BaseModel):
+    score: Annotated[int, "Adherence score from 0 to 10"]
+    reasoning: Annotated[str, "Reasoning behind the score."]
+
+class StyleScore(BaseModel):
+    score: Annotated[int, "Style score from 0 to 10"]
+    reasoning: Annotated[str, "Reasoning behind the score."]
+
+def adherence_evaluator(run:Run, example:Example) -> dict:
+    '''
+    This function is a custom evaluator for the adherence of the generated code.
+    Input parameters:
+    run: Run object containing the generated code and context.
+    example: Example object containing the reference code and question.
+
+    Returns:
+    dict: a dictionary containing the adherence score, the reasoning and the key.
+    '''
+    prediction = run.outputs["answer"],
+    reference = example.outputs["answer"]
+
+    adherence_test_template = '''
+        You are a strict and intelligent code evaluator. Your task is to assess the adherence of the predicted code with respect to the reference code.
+
+        Definition of "adherence":
+
+        - the predicted code has the same structure of the reference code;
+        - the predicted code implements the same logical steps or algorythm of the reference code;
+        - the predicted code produces the same output of the reference code given the same input.
+
+        What must *not* influence the adherence:
+        - variable names, even if they affect the code output;
+        - minor differences in the structure that doesn't change the meaning (e.g. using a loop instead of a list comprehension)
+        
+        Rate adherence on a scale from 0 to 10:
+            0: Incorrect logic or failure to implement the intended functionality.
+            6: Mostly equivalent but with minor logical inconsistencies.
+            10: Fully equivalent in logic and behaviour.
+        Return the score calculated (0-10).
+
+
+        Follow these steps:
+        1. Compare the predicted code and the context line-by-line or block-by-block.
+        2. Identify which parts of the code are aligned with the reference code.
+        3. Reason explicitly about whether the predicted code is adherent to reference code.
+
+        Here is the predicted code:
+        {prediction}
+
+        Here is the reference code:
+        {reference}
+
+        Return your analysis in the following **structured JSON format**:
+
+        {{
+        "score": <number from 0 to 10>,
+        "reasoning": "<clear explanation of why this score was given, pointing to specific parts of the code and reference>"
+        }}
+        '''
+    
+    adherence_test_prompt = PromptTemplate.from_template(adherence_test_template)
+    formatted_output_llm = llm.with_structured_output(AdherenceScore, method="json_mode")
+    # Create the evaluation chain
+    adherence_test_chain = (adherence_test_prompt | formatted_output_llm)
+    response = adherence_test_chain.invoke({"prediction": prediction, "reference": reference})
+
+    return {"score": response.score, "key": adherence_evaluator.__name__, "reasoning": response.reasoning}
+
+# TO BE UPDATED
 style_evaluator = LangChainStringEvaluator(
         "labeled_criteria",
         prepare_data= lambda run, example: {
@@ -100,17 +148,7 @@ style_evaluator = LangChainStringEvaluator(
         },
         }
     )
-
-# Evaluator for hallucinations
-from langsmith.schemas import Run, Example
-from langchain.prompts import PromptTemplate
-from pydantic import BaseModel
-from typing import Annotated
-class GroundnessScore(BaseModel):
-    score: Annotated[int, "Groundness score from 0 to 10"]
-    reasoning: Annotated[str, "Reasoning behind the score."]
-
-
+    
 def groundness_evaluator(run:Run, example:Example) -> dict:
     '''
     This function is a custom evaluator for the groundness of the generated code.
@@ -177,7 +215,7 @@ def execute_test_suite(test_type:list[TestType], dataset_name = dataset_name) ->
                 evaluate(
                     predict_rag_answer,
                     data = dataset_name,
-                    evaluators = [correctness_evaluator, style_evaluator],
+                    evaluators = [adherence_evaluator],
                     experiment_prefix= "correctness_test",
                 )
                 print("Correctness tests terminated.")
@@ -194,4 +232,4 @@ def execute_test_suite(test_type:list[TestType], dataset_name = dataset_name) ->
     except Exception as e:
         print(f"An unknown exception occurred:\n{e}")
 
-execute_test_suite([TestType.GROUNDNESS])
+execute_test_suite([TestType.CORRECTNESS])
